e1cf08580a2828b9efc2769de12f6a8e533c8d9a
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-19 17:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
+<<<<<<< HEAD
 function happyHolidays(string) {
   return ("Happy holidays!")
 }
@@ -13,4 +14,12 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return(`It's ${days} days until ${holiday}!`)
+=======
+function happyHolidays() {
+  return ("Happy Holidays")
+}
+
+function happyHolidaysTo(name) {
+  return ("Happy Holidays, ${name}")
+>>>>>>> a5db41dde0224e372b38871f0037c5e23d6d5629
 }
